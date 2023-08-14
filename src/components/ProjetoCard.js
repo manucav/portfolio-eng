ea4aa856423cb73241b7cc2c2bd17f1d4928d78e
manu <100045066+manucav@users.noreply.github.com>
2023-08-13 21:10:05 -0300

    att

--- src/components/ProjetoCard.js
+++ src/components/ProjetoCard.js
@@ -4,7 +4,7 @@ export const ProjetoCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={6}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt={`Image of project: ${title}`}/>
+        <img src={imgUrl} alt={`Project: ${title}`}/>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
